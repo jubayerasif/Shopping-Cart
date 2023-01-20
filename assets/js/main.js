1127c1c15e144f0afe6e01a163c296a17770bfc2
jubayerasif <jubaerasif101@gmail.com>
2023-01-21 01:54:21 +0600

    project completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,22 @@ function updateProductNumber(product, price, isIncreasing){
     let productNumber = productInput.value;
     if(isIncreasing == true){
         productNumber = parseInt(productNumber) + 1;
-    }
-     else if (productNumber > 0){
+    }else if (productNumber > 0){
         productNumber = parseInt(productNumber) - 1;
     }
     productInput.value = productNumber;
-
+    //Total updated
     const productTotal = document.getElementById(product + '-total');
     productTotal.innerText = productNumber * price;
-
+    //Total Calculated
     calculateTotal();
-}
+};
 
 function getInputValue(product){
     const productInput = document.getElementById(product + '-number');
     const productNumber = parseInt(productInput.value);
     return productNumber;
-}
+};
 
 function calculateTotal(){
     const phoneTotal = getInputValue('phone') * 1910;
@@ -28,7 +27,23 @@ function calculateTotal(){
     const tax = subTotal / 10;
     const totalPrice = subTotal + tax;
     // Html gets updated
-    document.getElementById('sub-total').innerHTML = subTotal;
-    document.getElementById('tax-amount'). innerHTML = tax;
-    document.getElementById('total-price'.innerHTML = totalPrice;)
-}
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+};
+
+// Event Handler of Phone Price Increase And Decrease
+document.getElementById('phone-plus').addEventListener('click', function (){
+    updateProductNumber('phone', 1910, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function(){
+    updateProductNumber('phone', 1910, false);
+});
+
+// Event Handler of Case Price Increase And Decrease
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateProductNumber('case', 49, true);
+});
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateProductNumber('case', 49, false);
+});
