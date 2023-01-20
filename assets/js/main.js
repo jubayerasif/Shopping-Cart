a3feaddb1fee468fec968382256ef7433febbafb
jubayerasif <jubaerasif101@gmail.com>
2023-01-21 01:41:27 +0600

    making js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ function updateProductNumber(product, price, isIncreasing){
     let productNumber = productInput.value;
     if(isIncreasing == true){
         productNumber = parseInt(productNumber) + 1;
-    } else if (productNumber > 0){
+    }
+     else if (productNumber > 0){
         productNumber = parseInt(productNumber) - 1;
     }
     productInput.value = productNumber;
@@ -17,4 +18,17 @@ function updateProductNumber(product, price, isIncreasing){
 function getInputValue(product){
     const productInput = document.getElementById(product + '-number');
     const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1910;
+    const caseTotal = getInputValue('case') *  49;
+    const subTotal = phoneTotal +caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // Html gets updated
+    document.getElementById('sub-total').innerHTML = subTotal;
+    document.getElementById('tax-amount'). innerHTML = tax;
+    document.getElementById('total-price'.innerHTML = totalPrice;)
 }
